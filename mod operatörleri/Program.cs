d456cc9a7b0dbdc8243898266ce5d7d6bec18c4f
sefaz <sefaz@SEFA>
2024-07-22 16:43:57 +0300

    Proje dosyası ekle.

---
+++ mod operatörleri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod_operatörleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // mod alma operatörü : %
+            int sayi1 = 10;
+            int sayi2 = 3;
+            Console.WriteLine(sayi1%sayi2);
+            Console.ReadLine();
+            
+        }
+    }
+}
