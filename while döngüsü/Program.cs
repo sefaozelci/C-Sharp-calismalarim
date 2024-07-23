b37eab684d364d69889e5b174d53976e21a88377
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 16:45:35 +0300

    while döngüsü örnek

---
+++ while döngüsü/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_döngüsü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 den 10 a kadar olan sayıları ekrana yazdırın 
+
+            // int a = 1;
+
+            //while (a<=10)
+            //{
+            // Console.WriteLine(a);
+            // a+=1;
+
+            //  }
+            // Console.ReadLine();
+
+
+            //100 den 0 a kadar olan sayıları çift olanlarını büyükten küçüğe yazdırın 
+
+            int a = 100;
+            while(a>=0)
+            {
+                Console.WriteLine(a);
+                a -= 2;
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
