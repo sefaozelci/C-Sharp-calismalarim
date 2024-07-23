fee67257bd7407825d9b030ca14ab1b4de390c39
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 09:41:55 +0300

    arttırma ve azaltma operatörleri

---
+++ Atama Operatörleri/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atama_Operatörleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  atama operatörleri : =
+            int yas = 21;
+            Console.WriteLine(yas);
+            Console.ReadLine();
+            
+
+
+
+
+        }
+    }
+}
