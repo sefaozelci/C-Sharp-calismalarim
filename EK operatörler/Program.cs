24cde051851a39f43b6e83f21c51f992d57fb96f
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 10:19:59 +0300

    EK Operatörler

---
+++ EK operatörler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EK_operatörler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // +=
+            // -=
+            // /=
+            // *=
+
+            int a = 5;
+            a = a + 4;
+
+            int b = 5;
+            b -= 3;
+
+            int c = 8;
+            c /= 2;
+
+            int d = 10;
+            d *= 2;
+            Console.WriteLine(" d nin degeri:"+d);
+            Console.WriteLine("c nin degeri:"+c);
+            Console.WriteLine("b nin degeri:"+b);
+            Console.WriteLine("a nın degeri:"+a);
+            Console.ReadLine();
+
+
+        }
+    }
+}
