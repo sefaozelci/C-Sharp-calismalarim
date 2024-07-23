c35075126a1f8838193ed65ce0de29f15552f970
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 10:00:12 +0300

    küçüktür ve küçük eşittir operatörü

---
+++ küçüktür ve küçük eşittir operatörü/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace küçüktür_ve_küçük_eşittir_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // küçüktür operatörü
+            // Küçük Eşit Operatörü
+            int yas = 18;
+            bool sonuc = yas < 19;
+            //bool sonuc= yas <= 19; (aynı değeri verir)
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
