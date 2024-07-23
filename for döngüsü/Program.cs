7f819ece8ac28bcdeb17e2c6e8d3abdf9237944f
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 16:25:03 +0300

    for döngüdü örnek

---
+++ for döngüsü/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_döngüsü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 den 10 a kadar olan sayıları alt alta ekrana yazdıran programı kodlayınız
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+           //     Console.WriteLine(i);
+           //Console.ReadLine();
+
+
+            // 1 den 10 a kadar olan sayılardan çift olanları ekrana yazdıran programı kodlayınız
+
+            for(int i=1; i<=10; i=i+1)
+            {
+                if(i%2==0)
+                {
+                Console.WriteLine(i);
+                }
+                
+                
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
