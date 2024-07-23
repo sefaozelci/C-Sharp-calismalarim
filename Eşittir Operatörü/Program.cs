afafb1f8738d7474decf34d4f761bb904110789a
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 09:48:13 +0300

    Eşittir Operatörü

---
+++ Eşittir Operatörü/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eşittir_Operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // eşittir operatörü : ==
+            int sayi1 = 4;
+            int sayi2 = 4;
+            bool kontrolet = sayi1 == sayi2;
+            Console.WriteLine(kontrolet);
+            Console.ReadLine();
+
+        }
+    }
+}
