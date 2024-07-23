643b4831d318b3cf13695045f6e7c485da489272
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 09:51:58 +0300

    eşit değildir operatörü

---
+++ Eşit değildir operatörü/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eşit_değildir_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 3;
+            int sayi2 = 4;
+            bool kontrolEt = sayi1 != sayi2;
+            Console.WriteLine(kontrolEt);
+            Console.ReadLine();
+
+        }
+    }
+}
