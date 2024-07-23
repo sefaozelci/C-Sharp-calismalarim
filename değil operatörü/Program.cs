702c03eb84ece873a2226b8184a4af5781cca7ac
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 10:13:35 +0300

    değil operatörü

---
+++ değil operatörü/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace değil_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // değik operatörü : !
+
+            bool a = true;
+
+            Console.WriteLine(!a);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
