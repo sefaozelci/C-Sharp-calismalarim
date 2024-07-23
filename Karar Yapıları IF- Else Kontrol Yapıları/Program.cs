486e9d323ee0221af416b420ea26666791012542
Sefa Özelçi <sefazi686@outlook.com>
2024-07-23 13:36:53 +0300

    Karar Yapıları

---
+++ Karar Yapıları IF- Else Kontrol Yapıları/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karar_Yapıları_IF__Else_Kontrol_Yapıları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // öğrencinin notu 50 den büyükse başarılı küçükse başarısız yazdıran program çıktısını kodlayınız
+
+            Console.WriteLine("notu giriniz:");
+            float notu = float.Parse(Console.ReadLine());
+
+            if (notu >= 50 && notu<=85)
+            {
+                Console.WriteLine("Başarılı");
+            }
+            else if (notu >= 85 && notu<=100)
+            {
+                Console.WriteLine("Çok Başarılı");
+            }
+            else if(notu <50 && notu>=0)
+            {
+                Console.WriteLine("başarısız");
+            }
+            else
+            {
+                Console.WriteLine("yanlış giriş");
+            }
+            Console.ReadLine();
+                    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
