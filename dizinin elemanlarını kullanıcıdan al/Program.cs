09c4898597f04e9ee9d82bdcc1de40576093e760
Sefa Özelçi <sefazi686@outlook.com>
2024-07-24 10:59:51 +0300

    indeksleri input olarak alma

---
+++ dizinin elemanlarını kullanıcıdan al/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizinin_elemanlarını_kullanıcıdan_al
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[4];
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine("dizinin " + (i + 1) + " indeksini giriniz:");
+                sayilar[i] = int.Parse(Console.ReadLine());
+                
+
+
+            }
+
+            for (int i = 0;i < sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]+);
+
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+            }
+    }
+}
