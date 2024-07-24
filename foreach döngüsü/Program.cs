243cf833029c7d9971deea2a6c886e3fdf17d3fa
Sefa Özelçi <sefazi686@outlook.com>
2024-07-24 11:19:02 +0300

    foreach döngüsü

---
+++ foreach döngüsü/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach_döngüsü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach döngüsü
+
+           // string[] isimler = { "Enes", "Ahmet", "Sefa", "Eren" };
+
+           // foreach (string s in isimler)
+           // {
+           //     Console.WriteLine(s);
+           // }
+           // Console.ReadLine();
+
+            double[] os = { 1.2, 3.5, 4.6, 7.8 };
+
+            foreach(double d in os)
+            {
+                Console.WriteLine(d);
+            }
+            Console.ReadLine();
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+            }
+    }
+}
