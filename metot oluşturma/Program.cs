1570b1e7392c7fe365032a16144c9256c0732593
Sefa Özelçi <sefazi686@outlook.com>
2024-07-24 15:37:52 +0300

    metot oluşturma

---
+++ metot oluşturma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot_oluşturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //parametre almayan geriye değer döndürmeyen metot Tanımlama
+            selamver();
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        static void selamver()
+        {
+            Console.WriteLine("Selamlar");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ örnek1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace örnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplamcift = 0;
+            int cift = 0;
+            int tek = 0;
+            Console.WriteLine("degerleri giriniz:");
+            int[] degerler= Console.ReadLine().Split(' ');
+            for (int i=0; i<degerler.Length; i++)
+            {
+                if (degerler % 2 == 0)
+                {
+                    
+                    cift++;
+                }
+                if (degerler%2!=0)
+                {
+                    tek++;
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
