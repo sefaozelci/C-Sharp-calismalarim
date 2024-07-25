93f8ae7dc3dc309c4f3c533a625c5814aa1f082b
Sefa Özelçi <sefazi686@outlook.com>
2024-07-25 11:23:46 +0300

    örnek

---
+++ input olarak alınan sayi tek mi cift mi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace input_olarak_alınan_sayi_tek_mi_cift_mi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanicadan alinan sayi tek mi cift mi 
+
+            Console.WriteLine("bir sayi giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+
+            tekmi(sayi);
+            Console.ReadLine();
+        }
+
+        static void tekmi(int a)
+        {
+            if (a % 2 != 0)
+            {
+                Console.WriteLine("sayi tektir");
+            }
+            else
+            {
+                Console.WriteLine("sayi ciftir");
+            }
+        }
+    }
+}
---
+++ input olarak alınan sayi tek mi cift mi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("input olarak alınan sayi tek mi cift mi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("input olarak alınan sayi tek mi cift mi")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f19407aa-87c2-465e-8d25-48dfd5f6e833")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
