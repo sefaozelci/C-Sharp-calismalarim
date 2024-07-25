440f606045aef12436587b7a254bb22532879277
Sefa Özelçi <sefazi686@outlook.com>
2024-07-25 11:09:23 +0300

    örnek

---
+++ metot örnek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace metot_örnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcıdan aldığımız sayının karesini alalım eğer karesi 25'ten büyükse;
+            // karesi 25'ten büyüktür
+            // değilse 25'ten küçüktür yazdırsın 
+
+            Console.WriteLine("Bir sayı giriniz:");
+            int girilensayi= int.Parse(Console.ReadLine());
+
+            int sonuc= kareal(girilensayi);
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        static int kareal(int a )
+        {
+            return a * a;
+        }
+        
+    }
+}
