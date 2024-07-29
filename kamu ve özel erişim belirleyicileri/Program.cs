bcd726850335d90005599c1325fc86d81e23c46f
Sefa Özelçi <sefazi686@outlook.com>
2024-07-29 10:57:12 +0300

    kamu ve özel erişim belirleyicileri

---
+++ kamu ve özel erişim belirleyicileri/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamu_ve_özel_erişim_belirleyicileri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // public -- private -- protected -- internal 
+
+            müşteri musteri1 = new müşteri();
+            musteri1.isim = "Enes";
+            musteri1.soyisim = "Bayram";
+            musteri1.maasdegeri = 2500;
+            musteri1.cinsiyet = "Erkek";
+            musteri1.musteribilgilerigoster();
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ kamu ve özel erişim belirleyicileri/müşteri.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamu_ve_özel_erişim_belirleyicileri
+{
+    public class müşteri
+    {
+        public string isim;
+
+        public string soyisim;
+
+        public int maasdegeri;
+
+        public string cinsiyet;
+        public int yas;
+
+        public void musteribilgilerigoster()
+        {
+            Console.WriteLine("ismi:"+isim);
+            Console.WriteLine("soyisim:"+soyisim);
+            Console.WriteLine("Maaş Değeri:"+maasdegeri);
+            Console.WriteLine("Yaş Değeri"+yas);
+        }
+
+        private void isimsoyisimyazdir(string musteriisim, string musterisoyisim)
+        {
+            Console.WriteLine("Müşterinin İsim ve Soyisimi:"+musteriisim+" "+ musterisoyisim);
+
+        }
+
+    }
+}
