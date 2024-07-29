cce0071dba81b0a24d0b9f2aea364968ac753999
Sefa Özelçi <sefazi686@outlook.com>
2024-07-29 09:48:23 +0300

    sınıf oluşturma

---
+++ Sınıf oluşturma/Araba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıf_oluşturma
+{
+    public class Araba
+    {
+        public int  kapisayisi;
+        public string arabamodel;
+        public string arabarengi;
+
+        public void motorcalistir()
+        {
+            Console.WriteLine("motor calisiyor");
+        }
+
+        public void kapilarikilitle()
+        {
+            Console.WriteLine("kapilar kitleniyor..");
+        }
+    }
+}
---
+++ Sınıf oluşturma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıf_oluşturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba();
+            Console.WriteLine(araba1);
+            Console.ReadLine();
+            araba1.kapisayisi = 4;
+            araba1.arabarengi = "kırmızı";
+            araba1.arabamodel = "bmw";
+
+            araba1.kapilarikilitle();
+            araba1.motorcalistir();
+
+            Console.WriteLine("arabanın kapi sayisi"+araba1.kapisayisi);
+            Console.WriteLine("araba rengi"+araba1.arabarengi );
+            Console.WriteLine("arabanın modeli"+araba1.arabamodel);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
